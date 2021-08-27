e1e383c88592b5f4b61b5fa8fc6f01914ab0ec33
Ahmetbugra <bugra.ahmetozcan@gmail.com>
2021-08-27 23:04:30 +0300

    "package.json güncellemeleri"

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const app = express();
 const flash = require('connect-flash');
+const path = require('path')
 const userController = require('./controllers/user_controller');
 const blogController = require('./controllers/blog_controller');
 const commentController = require('./controllers/comment_controller');
@@ -27,23 +28,37 @@ app.use(expressSession);
 app.use(express.static(__dirname));
 app.use(cookieParser());
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 const db = mongoose.connection;
 
 
 
-app.listen(9000, () => {
-    console.log("Listening port 9000");
-});
 
 
-
+app.use(express.static(path.join(__dirname, '../build')))
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../build'))
+// })
 
 app.use(userController.router);
 app.use(blogController.router);
 app.use(commentController.router);
 app.use(likeController.router);
+const port = process.env.PORT || 9000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
 // app.get("/", (req,res)=>{
 //     res.send({name: "ahmet"});
 //     console.log(process.env.DATABASE_URL)
 // })
+// server static assets if in production
+if(process.env.NODE_ENV === 'production'){    
+    app.use(express.static('frontend/build'))  // set static folder 
+    //returning frontend for any route other than api 
+    app.get('*',(req,res)=>{     
+        res.sendFile (path.resolve(__dirname,'frontend','build',         
+                      'index.html' ));    
+    });
+}
